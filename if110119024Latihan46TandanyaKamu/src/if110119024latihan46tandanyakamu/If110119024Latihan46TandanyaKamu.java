46ca2ac45a4df175c13754a14d86cc64b724dc90
kusnandi <72843119+kusnandi@users.noreply.github.com>
2020-11-14 22:49:53 +0700

    Add files via upload

---
+++ if110119024Latihan46TandanyaKamu/src/if110119024latihan46tandanyakamu/If110119024Latihan46TandanyaKamu.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110119024latihan46tandanyakamu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acer
+ * KUSNANDI PIRMANSYAH
+ * IF-1
+ * 10119024
+ */
+public class If110119024Latihan46TandanyaKamu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        Age umur = new Age(2020);
+        
+        System.out.print("Masukkan Tahun Lahir Anda : ");
+        umur.setYearBirth(scanner.nextInt());
+        
+        System.out.println();
+        System.out.println();
+        System.out.println("======Hasil Perhitungan Umur======");
+        int yearBirth = umur.getYearBirth();
+        System.out.println("Tahun Lahir Anda : " + yearBirth);
+        int yearNow = umur.getYearNow();
+        System.out.println("Hari ini Tahun : " + yearNow);
+        int usia = umur.hitungUmur();
+        System.out.println("Umur kamu sampai hari ini adalah " + usia + " tahun");
+        System.out.println("Tandanya Kamu " + umur.tandanyaKamu(usia));
+    }
+    
+}
